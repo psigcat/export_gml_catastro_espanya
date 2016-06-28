763eac0ea40401f1539bbc05f4f3e6dd6e01e042
carlospsig <carlos.lopez@psig.es>
2016-06-28 21:30:38 +0200

    Add __init_py and metadata initials

---
+++ __init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ export_gml_catastro_espanya - A QGIS plugin for cretae gml parcels for Spain's catastro
+                             -------------------
+        begin                : 2016-06-29
+        copyright            : (C) 2016 by Martí Angelats
+        email                : martiangelats@hotmail.com
+        git sha              : $Format:%H$
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
+
+
+# noinspection PyPep8Naming
+def classFactory(iface):  # pylint: disable=invalid-name
+    """Load export_gml_catastro_espanya class from file export_gml_catastro_espanya.
+
+    :param iface: A QGIS interface instance.
+    :type iface: QgsInterface
+    """
+    #
+    from .search_plus import export_gml_catastro_espanya
+    return export_gml_catastro_espanya(iface)
