92233f0a2eba72b7249a1fa4ceae24f326719f9a
Martí Angelats i Ribera <martiangelats@hotmail.com>
2016-07-30 12:44:17 +0200

    Traduccions a l'anglès i al català

---
+++ i18n/export_gml_catastro_espanya_ca.ts
@@ -0,0 +1,64 @@
+<!DOCTYPE TS>
+<TS>
+	<!-- Plugin file translation -->
+	<context>
+		<name>export_gml_catastro_espanya</name>
+		<message>
+			<source>Generar GML para el catastro español</source>
+			<translation>Generar GML pel catastre espanyol</translation>
+		</message>
+		<message>
+			<source>La capa seleccionada no utiliza un sistema de coodenadas compatible.</source>
+			<translation>La capa seleccionada no utilitza un sistema de coordenades compatible.</translation>
+		</message>
+		<message>
+			<source>La capa selccionada no contiene los campos necesarios.</source>
+			<translation>La capa seleccionada no conté els camps necesaris.</translation>
+		</message>
+		<message>
+			<source>Tipo de WKB no compatible.</source>
+			<translation>Tipus de WKB no compatible.</translation>
+		</message>
+		<message>
+			<source>Error</source>
+			<translation>Error</translation>
+		</message>
+	</context>
+
+	<!-- Plugin's popup dialog translation -->
+	<context>
+		<name>requestData_dialog</name>
+		<message>
+			<source>HERRAMIENTA PARA EXPORTAR A GML</source>
+			<translation>EINA PER EXPORTAR A GML</translation>
+		</message>
+		<message>
+			<source>Refcat</source>
+			<translation>Refcat</translation>
+		</message>
+		<message>
+			<source>0822805DF1802S</source>
+			<translation></translation>
+		</message>
+		<message>
+			<source>Numero parcela</source>
+			<translation>Numero de parcela</translation>
+		</message>
+		<message>
+			<source>Area (m²)</source>
+			<translation>Area (m²)</translation>
+		</message>
+		<message>
+			<source>Dia de edición</source>
+			<translation>Dia d'edició</translation>
+		</message>
+		<message>
+			<source>Guardar...</source>
+			<translation>Guardar...</translation>
+		</message>
+		<message>
+			<source>Cancelar</source>
+			<translation>Cancelar</translation>
+		</message>
+	</context>
+</TS>
---
+++ i18n/export_gml_catastro_espanya_en.ts
@@ -0,0 +1,64 @@
+<!DOCTYPE TS>
+<TS>
+	<!-- Plugin file translation -->
+	<context>
+		<name>export_gml_catastro_espanya</name>
+		<message>
+			<source>Generar GML para el catastro español</source>
+			<translation>Generate spanish' catastre GML file</translation>
+		</message>
+		<message>
+			<source>La capa seleccionada no utiliza un sistema de coodenadas compatible.</source>
+			<translation>The selected layer doesn't use a supported CRS.</translation>
+		</message>
+		<message>
+			<source>La capa selccionada no contiene los campos necesarios.</source>
+			<translation>The selected layed doesn't contain the required fields.</translation>
+		</message>
+		<message>
+			<source>Tipo de WKB no compatible.</source>
+			<translation>Unsopported WKB Type.</translation>
+		</message>
+		<message>
+			<source>Error</source>
+			<translation>Error</translation>
+		</message>
+	</context>
+
+	<!-- Plugin's popup dialog translation -->
+	<context>
+		<name>requestData_dialog</name>
+		<message>
+			<source>HERRAMIENTA PARA EXPORTAR A GML</source>
+			<translation>GML EXPORTING TOOL</translation>
+		</message>
+		<message>
+			<source>Refcat</source>
+			<translation>Refcat</translation>
+		</message>
+		<message>
+			<source>0822805DF1802S</source>
+			<translation></translation>
+		</message>
+		<message>
+			<source>Numero parcela</source>
+			<translation>Plot number</translation>
+		</message>
+		<message>
+			<source>Area (m²)</source>
+			<translation>Area (m²)</translation>
+		</message>
+		<message>
+			<source>Dia de edición</source>
+			<translation>Edit date</translation>
+		</message>
+		<message>
+			<source>Guardar...</source>
+			<translation>Save...</translation>
+		</message>
+		<message>
+			<source>Cancelar</source>
+			<translation>Cancel</translation>
+		</message>
+	</context>
+</TS>
